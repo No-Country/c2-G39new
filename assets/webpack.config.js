a2ce8bb4b6e59957349ac2cd26ceabc34a42a8d4
Edgar Ocampo <edgarocampo36@gmail.com>
2022-01-21 18:33:07 -0300

    Modificando estructura proyecto

---
+++ assets/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+//var webpack = require("webpack")
+var BundleTracker = require("webpack-bundle-tracker");
+
+module.exports = {
+  mode: "development",
+  context: __dirname,
+
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve('./bundles'),
+    filename: "[name]-[hash].js",
+  },
+
+  plugins: [new BundleTracker({ filename: "../django_react_demo/webpack-stats.json" })],
+
+  module: {
+    rules: [
+      { test: /\.js?$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.css$/, 
+        loader: "style-loader"
+      },
+      { test: /\.css$/, 
+        loader: "css-loader", 
+        options: {url: true, import: true}
+      },
+    ],
+  },
+};
