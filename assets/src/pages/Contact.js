93ebd05c4aac434163e961097612ac8a1736bfb2
Edgar Ocampo <edgarocampo36@gmail.com>
2022-01-28 15:42:20 -0300

    Se agregan datos en page Contacto

--- assets/src/pages/Contact.js
+++ assets/src/pages/Contact.js
@@ -9,13 +9,13 @@ export default class Home extends React.Component {
         <br />
         <h4><u>Matias Gonzalez</u></h4>
         <p>Desarrollador Frontend con React</p>
-        <p>Correo: <i></i></p>
-        <p>Linkedin: <i></i></p>
+        <p>Correo: mng_epson@hotmail.com<i></i></p>
+        <p>Linkedin: matias-gonzalez-61061a207<i></i></p>
         <br />
         <h4><u>Jonatan Alcaraz</u></h4>
         <p>Desarrollador Backend con Django</p>
-        <p>Correo: <i></i></p>
-        <p>Linkedin: <i></i></p>
+        <p>Correo: jonatan.alcaraz9@gmail.com<i></i></p>
+        <p>Linkedin: jonatanalcaraz<i></i></p>
         <br />
         <h4><u>Edgar Ocampo</u></h4>
         <p>Desarrollador Full Stack con React y Django</p>
